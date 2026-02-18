717eb546c8e76548b3258951628aaf64748f4fc9
vishwesh <vishwesharun@gmail.com>
2026-02-18 16:53:09 +0530

    force rebuild correct api route

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -83,6 +83,7 @@ const AuthModal: React.FC<AuthModalProps> = ({ isOpen, onClose, isDarkMode, user
         }
     };
 
+    
     const handleGoogleSignIn = async () => {
         setLoading(true);
         setMessage(null);
